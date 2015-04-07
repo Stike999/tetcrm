f41b1c9ca2f13b6f27fb20cd1b128e8a48738ae7
ivan-sabo <123sabo@gmail.com>
2015-04-07 16:32:06 +0200

    datagrid and migration test

--- src/Adit/Bundle/ProjektBundle/Migrations/Schema/v1_0/CreateProjectsEntityMigration.php
+++ src/Adit/Bundle/ProjektBundle/Migrations/Schema/v1_0/CreateProjectsEntityMigration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Acme\ProjektBundle\Migrations\Schema\v1_0;
+namespace Adit\ProjektBundle\Migrations\Schema\v1_0;
 
 use Doctrine\DBAL\Schema\Schema;
 use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
@@ -10,15 +10,58 @@ use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
 class CreateProjectsEntityMigration implements ExtendExtensionAwareInterface, Migration
 {
-    private $extendExtension;
-    
-    public function setExtendExtension (ExtendExtension $extendExtension)
-    {
-        $this->extendExtension = $extendExtension;
-    }
-    
+    /**
+     * @inheritdoc
+     */
     public function up(Schema $schema, QueryBag $queries)
     {
+        /**projectType table
+         *id int
+         *type string
+         */
+        $projectType = $schema->createTable('projectType');
+        $projectType->addColumn('id', 'integer', ['autoincrement' => true]);
+        $projectType->addColumn('type', 'string');
+        $projectType->setPrimaryKey(['id']);
+        
+        /**projectStatus table
+         *id int
+         *status string
+         */
+        $projectStatus = $schema->createTable('projectStatus');
+        $projectStatus->addColumn('id', 'integer', ['autoincrement' => true]);
+        $projectStatus->addColumn('status', 'string');
+        $projectStatus->setPrimaryKey(['id']);
+        
+        /**project table
+         *id
+         *name
+         *typeId {ref projectType}
+         *statusId {ref projectStatus}
+         *completed
+         */
+        $project = $schema->createTable('project');
+        $project->addColumn('id', 'integer', ['autoincrement' => true]);
+        $project->addColumn('name', 'string');
+        $project->addColumn('typeId', 'integer', ['notnull' => false]);
+        $project->addForeignKeyConstraint(
+            $schema->getTable('projectType'),
+            ['typeId'],
+            ['id'],
+            ['onUpdate' => null, 'onDelete' => null]
+        );
+        $project->addColumn('statusId', 'integer', ['notnull' => false]);
+        $project->addForeignKeyConstraint(
+            $schema->getTable('projectStatus'),
+            ['statusId'],
+            ['id'],
+            ['onUpdate' => null, 'onDelete' => null]
+        );
+        $project->addColumn('completed', 'float', ['notnull' => false]);
+        $project->addColumn('anticipatedExpenses', 'float', ['notnull' => false]);
+        $project->addColumn('anticipatedRevenues', 'float', ['notnull' => false]);
+        $project->setPrimaryKey(['id']);
+        
         
     }
 }
